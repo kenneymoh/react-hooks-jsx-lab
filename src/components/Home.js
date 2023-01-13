7e85a2b366a61637a66ee5f66cf7aca48563078b
kennedy <kenneyrit@gmail.com>
2023-01-13 15:19:39 +0300

    updated changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,11 @@ import React from "react";
 import { name, city } from "../data/data.js";
 
 function Home() {
+  return <div id="home">
+    <h1 style={{color= firebrick}}> Your {name} is a web developer from your {city}</h1>
+  </div>;
   // update the JSX being returned!
-  return <div>Home</div>;
+  
 }
 
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 function NavBar() {
+  <nav>
+    <div>
+      <a href="#home">Home</a>
+      <a href="#about">About</a>
+    </div>
+  </nav>
   // update the JSX being returned!
   return <nav>NavBar</nav>;
 }
